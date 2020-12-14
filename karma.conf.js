cad72fd1b26a38f69746c8654ee8d46b888bc53b
Rishabh Rajpal <14rishab@gmail.com>
2020-12-14 21:38:08 +0530

    first changes

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/zibel-assignment'),
+      dir: require('path').join(__dirname, './coverage/zibal-assignment'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
