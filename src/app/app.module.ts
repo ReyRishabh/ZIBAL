a4b714559db3896f38d6a9d0540be892dd8efe61
Rishabh Rajpal <14rishab@gmail.com>
2020-12-14 21:28:54 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './components/employees/employees.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-
 import { EmployeeService } from './employees.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from "@angular/material/dialog";
